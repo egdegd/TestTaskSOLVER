174d0d95424f26940961fd56658c5d330abc9258
egdegd <egd3700@mail.ru>
2021-09-17 14:02:06 +0300

    add README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # TestTaskSOLVER
 Тестовое задание к проекту "Circuit SAT Solver"
+
+### Сборка проекта:
+```
+./build.sh
+```
+
+### Запуск:
+```
+./script.sh <input_file>
+```
+<input_file> должен содержать описание схемы в формате BENCH
+
+#### Пример:
+```
+./script.sh circuit_sat.bench
+```
--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-input_len=$(python3 circuit_sat_to_sat.py circuit_sat.bench)
+input_len=$(python3 circuit_sat_to_sat.py $1)
 cadical/build/cadical -q formula.cnf | sed -n '1p'
-cadical/build/cadical -q formula.cnf | sed -n '2p' | awk -F "$input_len" '{print $1}'
+cadical/build/cadical -q formula.cnf | sed -n '2p' | awk -F $input_len '{print $1}'
